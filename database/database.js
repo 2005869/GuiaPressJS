06a6667e3fd889520425421013d78a2a3fcee3bf
2005869 <2005869@aluno.univesp.br>
2022-11-18 09:41:45 -0300

    set timezone from Brazil

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const mysqlpassword = require('./secrets');
 
 const connection = new Sequelize('guiapress', 'root', mysqlpassword, {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;

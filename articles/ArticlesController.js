551a7a1ddf0625a2be3b60bd43ea416970bf473c
2005869 <2005869@aluno.univesp.br>
2022-11-18 08:27:16 -0300

    admin articles new

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -7,6 +7,8 @@ router.get('/articles', (req, res) => {
     res.send('pagina de articles');
 });
 
-
+router.get('/admin/articles/new', (req, res) => {
+    res.send('pagina de criacao de novo artigo');
+});
 
 module.exports = router;

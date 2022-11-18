d4073fade972a2739572db65aa185541e9cf80a8
2005869 <2005869@aluno.univesp.br>
2022-11-18 09:42:17 -0300

    articles save

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send('pagina de articles');
 });
 
@@ -12,6 +14,21 @@ router.get('/admin/articles/new', (req, res) => {
         res.render('admin/articles/new', {categories: categories});
     });
     
+});
+
+router.post('/articles/save', (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles');
+    });
 });
 
 module.exports = router;

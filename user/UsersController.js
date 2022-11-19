8616c591c4664ee40d08eee356e8992a6ddebf8b
2005869 <2005869@aluno.univesp.br>
2022-11-19 14:43:53 -0300

    form user create

--- user/UsersController.js
+++ user/UsersController.js
@@ -13,5 +13,11 @@ router.get('/admin/users/create', (req, res) => {
     res.render('admin/users/create');
 });
 
+router.post('/users/create', (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    res.json({email, password})
+});
 
 module.exports = router;
